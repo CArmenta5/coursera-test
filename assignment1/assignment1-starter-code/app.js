6bc3241d827669dca5760f85159dfb8738b0866a
CArmenta5 <carlosraulap@outlook.com>
2020-05-07 21:30:10 -0500

    Assignment-1 Complete

---
+++ assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,36 @@
+(function(){
+'use strict'
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',LunchCheckController);
+LunchCheckController.$injection =['$scope']
+function LunchCheckController($scope){
+    $scope.dish="";
+    $scope.totalDishes=0;
+    $scope.stateOfCheck="Enjoy!";
+
+    $scope.howMany=function (){
+
+        var totalDishes=calculatNum($scope.dish);
+        if(totalDishes>3){
+            $scope.stateOfCheck="Too much!";
+        }else if(totalDishes>0){
+            $scope.stateOfCheck="Enjoy!";
+        }else{
+            $scope.stateOfCheck="Please enter data first";
+
+        }
+    }
+  
+    function calculatNum(string){
+        var total=0;
+        var dishes = string.split(",");
+        for (var i = 0; i < dishes.length; i++) {
+            if(dishes[i].length!=0 && dishes[i]!=" "){
+                total++;
+            }
+          }
+        return total;
+    }
+}
+
+})();
